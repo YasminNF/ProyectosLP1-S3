d4eb80e7edc4d840a69d1dddacaee2997001e677
Yasmin Ninataype <kapricornio126@gmail.com>
2020-07-16 12:02:31 -0500

    Prog001 - Prog002 - Prog003 listo

---
+++ src/Estructuras/Prog003.java
@@ -0,0 +1,33 @@
+
+package Estructuras;
+
+//@author NIOUX
+
+import java.util.Scanner;
+
+public class Prog003 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String cadena;
+        System.out.println("*****MANEJO DE CADENAS*****");
+        System.out.print("Leer cadena con nextLine():  ");
+        cadena = teclado.nextLine();
+        System.out.println("Valor inicial ingresado es:  " + cadena);
+        System.out.println("Mayúscula con toUpperCase():  " + cadena.toUpperCase());
+        System.out.println("Mayúscula con toLowerCase():  " + cadena.toLowerCase());
+        System.out.println("Elimina espacios con  con trim():  " + cadena.trim());
+        System.out.println("Subcadena con substring(int):  "+ cadena.substring(5));
+        System.out.println("Subcadena con substring(int,int):  "+ cadena.substring(5,10));
+        System.out.println("Tamaño de una cadena:  " + cadena.length());
+        System.out.println("Extraer letra:  " + cadena.charAt(0));
+        System.out.println("Extraer letra:  " + cadena.charAt(1));
+        System.out.println("Extraer letra:  " + cadena.charAt(2));
+        System.out.println("Extraer letra:  " + cadena.charAt(3));
+        System.out.println("Extraer letra:  " + cadena.charAt(4));
+        System.out.println("Extraer letra:  " + cadena.charAt(5));
+        System.out.println("Extraer letra:  " + cadena.charAt(6));
+        System.out.println("Última letra:  " + cadena.charAt(cadena.length()-1));
+        teclado.close();
+    }
+    
+}
