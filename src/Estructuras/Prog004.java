5808708729e13e286aac66602f462c2d70cc99dd
Yasmin Ninataype <kapricornio126@gmail.com>
2020-07-16 12:07:28 -0500

    Prog001 - Prog002 - Prog003 - Prog004 listo

---
+++ src/Estructuras/Prog004.java
@@ -0,0 +1,17 @@
+
+package Estructuras;
+
+//@author NIOUX
+//Mostrar un mensaje 10 veces.
+
+public class Prog004 {
+    public static void main(String[] args) {
+        int numero=1;
+        String mensaje = "Cristo viene pronto - Prepárate";
+        while (numero<=10) {            
+            System.out.println(mensaje);
+            numero++;
+        }
+    }
+    
+}
