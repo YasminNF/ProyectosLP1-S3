53719a2d6d0acb9056151f2a38ee8e92131bb780
Yasmin Ninataype <kapricornio126@gmail.com>
2020-07-19 20:26:02 -0500

    Prog011 - listo

---
+++ src/Recursividad/Prog011.java
@@ -0,0 +1,37 @@
+
+package Recursividad;
+
+//@author NIOUX
+
+import Utils.Leer;
+
+
+public class Prog011 {
+    public static void factorial1(int numero){
+        int fac=1;
+        for (int i = 1; i <= numero; i++) {
+            fac*=i;
+        }
+        System.out.println("Factorial01 de " + numero + ": " + fac);
+    }
+    public static int factorial2(int numero){
+        int fac;
+        if(numero==0){
+            fac=1;
+        }else{
+            fac = numero*factorial2(numero-1);
+        }
+        return fac;
+    }
+    
+    public static void inicio(){
+        System.out.print("Numero: ");
+        int numero = Leer.entero();
+        factorial1(numero);
+        System.out.println("Factorial02 de + " + numero + ": " + factorial2(numero) );
+        
+    }
+    public static void main(String[] args) {
+        inicio();
+    } 
+}
