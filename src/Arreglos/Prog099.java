7fe15220af3274d2a1332916455fc346fc76cf8d
Yasmin Ninataype <kapricornio126@gmail.com>
2020-07-19 20:33:19 -0500

    Prog099 - listo

---
+++ src/Arreglos/Prog099.java
@@ -0,0 +1,31 @@
+
+package Arreglos;
+
+//@author NIOUX
+
+import Utils.Leer;
+
+// Diseñar un algoritmo que permita el ingreso de 7 
+// valores a una arreglo y mostrarlos
+
+public class Prog099 {
+    public static void inicio(){
+        int[] a = new int[7];
+        System.out.println("Tamaño del arreglo: " + a.length);
+        System.out.println("Ingresar los elementos: ");
+        for (int i = 0; i < a.length ; i++) {
+            System.out.print("a[ "+i+" ]: ");
+            a[i] = Leer.entero();
+            
+        }
+        System.out.println("Los elementos del arreglo son: ");
+        for (int i = 0; i <  a.length; i++) {
+            System.out.println("a[ "+i+" ]: " + a[i]);
+            
+        }
+        
+    }
+    public static void main(String[] args) {
+        inicio();
+    }
+}
